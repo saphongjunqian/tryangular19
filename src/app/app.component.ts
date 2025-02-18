7c739f145c4b690a37977f9d6c3dfca8e433e659
Hongjun Qian <hongjun.qian@sap.com>
2025-02-18 08:40:34 +0800

    Update to angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,10 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    imports: [CommonModule, RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   isCollapsed = false;
